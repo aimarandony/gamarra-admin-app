8366b4703c79bff2f506b7d85b3d78c048e87a2b
Aimar Berrocal (NTTData) <aimarandony.berrocalcoaquira@nttdata.com>
2022-07-21 03:05:38 -0500

    Added Modal Sale

--- src/components/ProductShopCartList.js
+++ src/components/ProductShopCartList.js
@@ -4,13 +4,18 @@ import { Button, Drawer } from 'antd';
 import './ProductShopCart.css'
 import { ProductCartItem } from './ProductCartItem';
 
-export const ProductShopCartList = ({ openDrawer, setOpenDrawer, cart, setCart }) => {
+export const ProductShopCartList = ({ openDrawer, setOpenDrawer, cart, setCart, setOpenModal }) => {
 
     const [total, setTotal] = useState(0)
     const [deleteChange, setDeleteChange] = useState(0)
 
     const onClose = () => {
         setOpenDrawer(false);
+    }
+
+    const handleOpenModal = () => {
+        setOpenDrawer(false);
+        setOpenModal(true);
     }
 
     useEffect(() => {
@@ -29,11 +34,11 @@ export const ProductShopCartList = ({ openDrawer, setOpenDrawer, cart, setCart }
                     <span className='text'>Pago Total</span>
                     <span className='price'>S/ {total}</span>
                 </div>
-                <Button size='large' type='primary' block>COMPRAR</Button>
+                <Button onClick={handleOpenModal} size='large' type='primary' block>COMPRAR</Button>
             </div>}
         >
             {
-                cart.map((product, index) => (<ProductCartItem key={product.id} product={product} cart={cart} setCart={setCart} position={index} setDeleteChange={setDeleteChange}/>))
+                cart.map((product, index) => (<ProductCartItem key={product.id} product={product} cart={cart} setCart={setCart} position={index} setDeleteChange={setDeleteChange} />))
             }
         </Drawer>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,13 @@ import { ShoppingFilled } from '@ant-design/icons'
 import { ProductShopCartList } from '../components/ProductShopCartList'
 
 import './Home.css'
+import { SaleModal } from '../components/SaleModal'
 
 export default function Home() {
 
     const [data, setData] = useState([]);
     const [openDrawer, setOpenDrawer] = useState(false);
+    const [openModal, setOpenModal] = useState(true);
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
@@ -21,11 +23,12 @@ export default function Home() {
         <div className='c-home container'>
             <ProductShopList productList={data} cart={cart} setCart={setCart}/>
             <div className='c-button-car'>
-                <span className='count'>6</span>
+                <span className='count'>{cart.length}</span>
                 <Button type='primary' shape='circle' onClick={() => setOpenDrawer(true)}
                     icon={<ShoppingFilled style={{ display: 'inline-flex' }} />} size='large' />
             </div>
-            <ProductShopCartList openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} cart={cart} setCart={setCart} />
+            <ProductShopCartList openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} cart={cart} setCart={setCart} setOpenModal={setOpenModal} />
+            <SaleModal openModal={openModal} setOpenModal={setOpenModal} cart={cart} setCart={setCart}/>
         </div>
     )
 }
